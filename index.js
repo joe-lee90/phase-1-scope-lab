4f6455ac970b6c3366a5228c28ae0527d066d520
Joe <zvmboni@gmail.com>
2022-03-16 08:34:35 -0700

    commit tree 4a02d53d09441809ed62f0aaad38084346c10b57

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase(); 
+}
+
+function setBestCustomer(){
+    // variables with no type are always in the global scope
+    // never want to do this
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'karen';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'joe';
+}
